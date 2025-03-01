b6d80543f77f80d534170ddc6e4367b1fc856af1
noahware <131519316+noahware@users.noreply.github.com>
2025-03-01 14:50:17 +0000

    Sorted hash wrapper structure into separate file.
    Sorted hash wrapper structure into separate file.

--- kernel/src/crypto/crypto.h
+++ kernel/src/crypto/crypto.h
@@ -1,17 +1,8 @@
 #pragma once
-#include "generic_types.h"
-#include "../context/context.h"
+#include "crypto_def.h"
 
 namespace crypto
 {
-	struct s_hash
-	{
-		uint8_t* buffer;
-		uint32_t buffer_size;
-
-		void free(context::s_context* context);
-	};
-
 	uint64_t xor64(uint64_t input, uint64_t key);
 
 	int32_t sha256(context::s_context* context, uint8_t* buffer, uint32_t buffer_size, uint8_t** hash_buffer, uint32_t* hash_size);
