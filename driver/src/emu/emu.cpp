f8b8091b127f0baf1b4198b47d5d00e5a6db413d
noahware <131519316+noahware@users.noreply.github.com>
2026-06-25 09:51:48 +0100

    Removed memory manager check + used Zw instead of Nt call

--- driver/src/emu/emu.cpp
+++ driver/src/emu/emu.cpp
@@ -6,10 +6,11 @@
 
 #include <ntddk.h>
 
+#include "../log.hpp"
 #include "../krnl/krnl.hpp"
 #include "../krnl/types.hpp"
 
-using nt_system_debug_control_t = NTSTATUS(NTAPI*)(ULONG, PVOID, ULONG, PVOID, ULONG, PULONG);
+using zw_system_debug_control_t = NTSTATUS(NTAPI*)(ULONG, PVOID, ULONG, PVOID, ULONG, PULONG);
 
 [[nodiscard]] static bool check_dbgprompt()
 {
@@ -58,69 +59,22 @@ using nt_system_debug_control_t = NTSTATUS(NTAPI*)(ULONG, PVOID, ULONG, PVOID, U
 [[nodiscard]] static bool check_debugger_status_funcs()
 {
 	UNICODE_STRING name = { };
-	RtlInitUnicodeString(&name, L"NtSystemDebugControl");
+	RtlInitUnicodeString(&name, L"ZwSystemDebugControl");
 
-	const auto NtSystemDebugControl = static_cast<nt_system_debug_control_t>(
+	const auto ZwSystemDebugControl = static_cast<zw_system_debug_control_t>(
 		MmGetSystemRoutineAddress(&name)
 	);
 
-	return NtSystemDebugControl(0, nullptr, 0, nullptr, 0, nullptr) != STATUS_DEBUGGER_INACTIVE;
-}
+	if (!ZwSystemDebugControl)
+	{
+		return false;
+	}
 
-[[nodiscard]] static _MMPFN* get_mm_pfn_entry(const uintptr_t phys_addr)
-{
-	const uintptr_t pfn = phys_addr >> PAGE_SHIFT;
-
-	return &krnl::mm_pfn_database[pfn];
-}
-
-[[nodiscard]] static uint32_t get_mm_pfn_ref_count(const uintptr_t phys_addr)
-{
-	const _MMPFN* const entry = get_mm_pfn_entry(phys_addr);
-
-	return entry->u3.ReferenceCount;
-}
-
-[[nodiscard]] static bool check_memory_manager_behavior()
-{
-	/*
-		allocate physical page
-		check it is in mmpfndatabase
-		mmmapiospace that physical page, check if referencecount increments in mmpte
-	*/
-
-	constexpr PHYSICAL_ADDRESS low = { .QuadPart = 0 };
-	constexpr PHYSICAL_ADDRESS high = { .QuadPart = -1 };  // no upper bound
-	constexpr PHYSICAL_ADDRESS skip = { .QuadPart = 0 };
-
-	// todo: handle cases where other things could tamper with this physical address we've just mapped
-	const PMDL mdl = MmAllocatePagesForMdlEx(
-		low, high, skip,
-		PAGE_SIZE,          // one page
-		MmCached,
-		MM_ALLOCATE_FULLY_REQUIRED
-	);
-
-	const auto pfn_array = MmGetMdlPfnArray(mdl);
-	const uintptr_t phys_addr = pfn_array[0] << PAGE_SHIFT;
-
-	const auto original_ref_count = get_mm_pfn_ref_count(phys_addr);
-
-	void* const map = MmMapIoSpace(PHYSICAL_ADDRESS{ .QuadPart = static_cast<LONGLONG>(phys_addr) }, PAGE_SIZE, MmCached);
-
-	const auto mapped_ref_count = get_mm_pfn_ref_count(phys_addr);
-
-	MmUnmapIoSpace(map, PAGE_SIZE);
-	MmFreePagesFromMdl(mdl);
-	ExFreePool(mdl);
-
-	const auto expected_ref_count = mapped_ref_count + 1;
-
-	return original_ref_count != expected_ref_count;
+	return ZwSystemDebugControl(0, nullptr, 0, nullptr, 0, nullptr) != STATUS_DEBUGGER_INACTIVE;
 }
 
 bool emu::is_emulated()
 {
 	return check_dbgprompt() || check_dbgctl_unchanged() || check_user_shared_data() || check_nt_debugger_fields() ||
-		check_debugger_status_funcs() || check_memory_manager_behavior();
+		check_debugger_status_funcs();
 }
