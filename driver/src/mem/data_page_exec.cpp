3ecb686d1cb99709a9484d9919a557a7e1436265
noahware <131519316+noahware@users.noreply.github.com>
2026-07-02 09:09:48 +0100

    Added validation of headers being made executable in kernel pages

--- driver/src/mem/data_page_exec.cpp
+++ driver/src/mem/data_page_exec.cpp
@@ -12,9 +12,36 @@ struct data_page_exec_info
     uint32_t rva;
 };
 
+static void process_memory_range(cstd::vector<data_page_exec_info>& infos, const uintptr_t image_base,
+                                 const uint32_t rva, const uint32_t size, const bool non_paged)
+{
+    const uint32_t end_rva = rva + size;
+
+    for (uint32_t i = rva; i < end_rva; i += mem::page_size)
+    {
+        const auto page_flags = mem::virt_page_flags(mem::curr_cr3(), image_base + i);
+
+        // todo: maybe communicate page flags==none to server
+        if (non_paged && page_flags == page_none)
+        {
+            DBG_LOG("unable to get page flags for kernel virt addr 0x%llx\n", image_base + i);
+        }
+
+        if (page_flags & page_execute)
+        {
+            infos.emplace_back(image_base, i);
+        }
+    }
+}
+
 static void collect_exec_data_pages(cstd::vector<data_page_exec_info>& infos, const krnl::module& mod)
 {
     const auto img = mod.image();
+    const uintptr_t base_addr = mod.base_address();
+
+    const uint32_t hdr_size = img->nt_headers()->optional_header.size_of_headers;
+
+    process_memory_range(infos, base_addr, 0, hdr_size, false);
 
     for (const auto& sec : img->sections())
     {
@@ -23,25 +50,7 @@ static void collect_exec_data_pages(cstd::vector<data_page_exec_info>& infos, co
             continue;
         }
 
-        const uintptr_t base_addr = mod.base_address();
-        const uint32_t rva = sec.virtual_address;
-        const uint32_t end_rva = rva + sec.virtual_size;
-
-        for (uint32_t i = rva; i < end_rva; i += mem::page_size)
-        {
-            const auto page_flags = mem::virt_page_flags(mem::curr_cr3(), base_addr + i);
-
-            // todo: maybe communicate page flags==none to server
-            if (sec.characteristics.mem_not_paged && page_flags == page_none)
-            {
-                DBG_LOG("unable to get page flags for kernel virt addr 0x%llx\n", base_addr + i);
-            }
-
-            if (page_flags & page_execute)
-            {
-                infos.emplace_back(base_addr, i);
-            }
-        }
+        process_memory_range(infos, base_addr, sec.virtual_address, sec.virtual_size, sec.characteristics.mem_not_paged);
     }
 }
 
